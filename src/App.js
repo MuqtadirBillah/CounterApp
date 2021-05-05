e03511e1bf29224e66b131c4d3adf533ba3c3a63
Muqtadir Billah <>
2021-05-06 03:26:26 +0500

    deploy counter app on github

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  if(count<0){
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      
+    <div className="appDiv">
+      <h1>Counter</h1>
+      <div className="counterDiv">
+        <h2 className="countNumber">{count}</h2>
+        <div className="buttonDiv">
+          <button className="buttonTag dec" onClick={()=>{setCount(count-1)}}><h1>-</h1></button>
+          <button className="buttonTag inc" onClick={()=>{setCount(count+1)}}><h1>+</h1></button>
+        </div>
+      </div>
+      <hr />
+      <p>Made by MBMA</p>
     </div>
   );
 }
